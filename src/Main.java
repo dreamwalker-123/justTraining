0ee02ab6f16c5ea1a6e0acdbc602c3ae0efdbfd1
dreamwalker-123 <125174202+dreamwalker-123@users.noreply.github.com>
2023-03-17 12:38:01 +0800

    обновка

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         //Напишите Java-программу для удаления определенного элемента из массива.
-        int[] arr = new int[]{12,34,65,23,76,87,26,84,41,74};
-        
+        int[] arr1 = new int[]{12,34,65,23,76,87,26,84,41,74};
+        System.out.println(Arrays.toString(remove(arr1, 4)));
+    }
+    public static int[] remove(int[] arr, int i) {
+        int[] result = new int[arr.length];
+        for (int j = 0; j < arr.length; j++) {
+            if (j != i+1) {
+                result[j] = arr[j];
+            } else {
+                for (int x = j; x < arr.length; x++) {
+                    result[x-1] = arr[x];
+                }
+                return result;
+            };
+        }
+        return result;
     }
 }
