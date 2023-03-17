84e46c3f2c2574072e6ac22f81513954da3a12a2
dreamwalker-123 <125174202+dreamwalker-123@users.noreply.github.com>
2023-03-17 11:26:08 +0800

    делаю упражнявки

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        //Напишите Java-программу для удаления определенного элемента из массива.
+        int[] arr = new int[]{12,34,65,23,76,87,26,84,41,74};
+        
+    }
+}
